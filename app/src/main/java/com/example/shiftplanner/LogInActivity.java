c2cc2a9486b14a4ac4085f8bbe179b205864208f
Krissi97 <86771852+Krissi97@users.noreply.github.com>
2021-09-08 12:53:54 +0200

    firstupload

---
+++ app/src/main/java/com/example/shiftplanner/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.shiftplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logInActivity);
+    }
+}
