4048dd04f227cf63852271a916376fef06d77964
med61603 <86771721+Dena00@users.noreply.github.com>
2021-09-14 16:50:57 +0200

    add CalenderViewHolder, CalendarAdapter and the layouts: calendar_cell.xml  & shift.xml

--- app/src/main/java/com/example/shiftplanner/ShiftActivity.java
+++ app/src/main/java/com/example/shiftplanner/ShiftActivity.java
@@ -3,6 +3,7 @@ package com.example.shiftplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ShiftActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class ShiftActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shift);
+    }
+
+    public void previousMonthAction(View view) {
+    }
+
+    public void nextMonthAction(View view) {
     }
 }
